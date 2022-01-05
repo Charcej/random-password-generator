ec7664d66de1e7bd27bd2bd167be4879d7536125
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-05 14:28:45 -0500

    character type and length

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// ask for character length
+var characterLength = Number(prompt("Enter a character length: between 8 and 128"));
+
+//ask for character types to include
+var characterType = prompt("Enter one character type to use: lowercase, uppercase, numeric or special)");
+
+//generate pw
+function generatePassword() {
+  // eval character type
+  var charSet = "";
+  var characterTypeLower = characterType.toLowerCase();
+  if (characterTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwyz";
+  } else if( characterTypeLower === "uppercase") {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( characterTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( characterTypeLower === "special ") {
+    charSet = " !@#$%^&*()_-+={[}]|\:;'<,>.?/~`";
+  }
+  //return
+  var returnValue = "";
+  for (var i = 0; i < characterLength; i++) {
+    // picks a character from charSet at index of random number
+    returnValue += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return returnValue;
+}
+console.log(generatePassword());
+  
+
+
 // Assignment code here
 
 
