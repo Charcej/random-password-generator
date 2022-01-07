a61373d84cae5b31f323de82649cd21d78b19430
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-07 14:14:22 -0500

    prompts and minumums

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
-// ask for character length
-var characterLength = Number(prompt("Enter a character length: between 8 and 128"));
-
-//ask for character types to include
-var characterType = prompt("Enter one character type to use: lowercase, uppercase, numeric or special)");
-
-//generate pw
-function generatePassword() {
-  // eval character type
-  var charSet = "";
-  var characterTypeLower = characterType.toLowerCase();
-  if (characterTypeLower === "lowercase" ) {
-    charSet = "abcdefghijklmnopqrstuvwyz";
-  } else if( characterTypeLower === "uppercase") {
-    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if( characterTypeLower === "numeric" ) {
-    charSet = "0123456789";
-  } else if( characterTypeLower === "special ") {
-    charSet = " !@#$%^&*()_-+={[}]|\:;'<,>.?/~`";
-  }
-  //return
-  var returnValue = "";
-  for (var i = 0; i < characterLength; i++) {
-    // picks a character from charSet at index of random number
-    returnValue += charSet.charAt(Math.floor(Math.random() * charSet.length));
-  }
-  return returnValue;
-}
-console.log(generatePassword());
-  
-
 
 // Assignment code here
-
-
-// Get references to the #generate element
+// Assigns a value to a variable
+// Gets the first element in the document with id="generate"
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Special characters for the function created
+const specialCharacters = " ~`@#$%^&*()_-+={[}]|\;:'<,>.?/ ";
+
+// Function to write password to the id = "password" input  (Writes pw to #password input)
 function writePassword() {
   var password = generatePassword();
+  // Gets the first element in the document with id = "passport"
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+// Should start whole process if all goes correctly
 generateBtn.addEventListener("click", writePassword);
+
+//Prompts that should come up when user presses the 'generate password' button 
+function generatePassword () {
+  var passwordLength = prompt("Howdy! How many characters do you want to include in your password? Pick a number between 8 and 128.");
+//From here on it's yes or no answers so use 'confirm'
+  var numbers = confirm("Do you want to include at least one number in your password?");
+
+  var lowerCases = confirm("Do you want to include at least one lowercase character?");
+
+  var upperCases = confirm("Do you want to include at least one uppercase character?");
+
+  var specialCharacters = confirm("Do you want to include at least one special character?");
+}
+
+// Minimum count for user input
+var minumumCount = 0;
+
+// Empty minumum for user input
+var minimumNumbers = "";
+var minimumLowerCases = "";
+var minimumUpperCases = "";
+var minimumSpecialCharacters = "";
