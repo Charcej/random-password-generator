35066db90cc4f649cb03b2541dc1869b9825192b
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-07 20:46:59 -0500

    completely changed code and after many tweaks it works

--- script.js
+++ script.js
@@ -1,103 +1,63 @@
+var characterLength = 8;
+var choiceArr = [];
 
-// Assignment code here
-// Assigns a value to a variable
-// Gets the first element in the document with id="generate"
+var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '|', '?', '/', '<', '>'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+// Assignment code here, assigns a value to a variable, gets the first element in the document with id="generate"
 var generateBtn = document.querySelector("#generate");
 
-// Special characters for the function created
-const specialCharacters = " ~`@#$%^&*()_-+={[}]|\;:'<,>.?/ ";
-
-// Function to write password to the id = "password" input  (Writes pw to #password input)
-function writePassword() {
-  var password = generatePassword();
-  // Gets the first element in the document with id = "passport"
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-// Should start whole process if all goes correctly
+// Add event listener to generate button, should start whole process if all goes correctly
 generateBtn.addEventListener("click", writePassword);
 
-//Prompts that should come up when user presses the 'generate password' button 
-function generatePassword () {
-  var passwordLength = prompt("Howdy! How many characters do you want to include in your password? Pick a number between 8 and 128.");
-//From here on it's yes or no answers so use 'confirm'
-  var numbers = confirm("Do you want to include at least one number in your password?");
+// Function to write password to the id = "password" input  
+function writePassword() {
+  var correctPrompts = getPrompts(); //true or false
+  var passwordText = document.querySelector("#password");
 
-  var lowerCases = confirm("Do you want to include at least one lowercase character?");
+  if(correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
 
-  var upperCases = confirm("Do you want to include at least one uppercase character?");
-
-  var special = confirm("Do you want to include at least one special character?");
-};
-
-// Minimum count for user input
-var minumumCount = 0;
-
-// Empty minumum for user input
-var minimumNumbers = "";
-var minimumLowerCases = "";
-var minimumUpperCases = "";
-var minimumSpecialCharacters = "";
-
-//Generator functions
-var functionArray = [
-  function getNumbers() {
-    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-  },
-
-  function getLowerCases() {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  },
-  function getUpperCases() {
-    return +String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  },
-  function getSpecialCharacters() {
-    return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
   }
 
-];
-
-// Makes sure user selected OK and fulfilled empty minimums
-
-if (numbers === true) {
-  minimumNumbers = functionArray[0];
-  minimumCount++;
+function generatePassword() {
+  // generatePassword based on prompts
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
 }
 
-if (lowerCases === true) {
-  minumumLowerCases = functionArray[1];
-}
+// Get character length and turn it into a number instad of a string
+function getPrompts(){
+  choiceArr = [];
+  characterLength = parseInt(prompt("How many characters long do you want your password to be? Pick a number between 8 and 128."));
 
-if (upperCases === true) {
-  minimumUpperCases = functionArray[2];
-}
+// Makes sure it's a number and that it's over 8 and under 128
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length has to be a number between 8 and 128. Please try again.");
+    return false;
+  }
 
-if (special === true) {
-  minimumSpecialCases = functionArray[3];
-  minimumCount++;
-}
-
-// Empty string variable for the for loop
-var randomPasswordGenerated = "";
-
-// For loop for getting all the random characters
-for (let i = 0; i < parseInt(passwordLength) - minimumCount; i++) {
-  var randomNumberPicked = Math.floor(Math.random () * 4);
-
-  randomPasswordGenerated += functionArray[randomNumberPicked] ();
-}
-
-// ensure characters added to pw
-randomPasswordGenerated += minimumNumbers;
-randomPasswordGenerated += minumumLowerCases;
-randomPasswordGenerated += minimumUpperCases;
-randomPasswordGenerated += minimumSpecialCharacters;
-
-return randomPasswordGenerated;
-
-
-
-
+// Don't have to worry about checking next four because they're true or false
+  if (confirm("Would you like lowercase letters?")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (confirm("Would you like uppercase letters?")) {
+      choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if (confirm("Would you like special characters?")) {
+      choiceArr = choiceArr.concat(specialCharArr);
+  }
+  if (confirm("Would you like numbers?")) {
+      choiceArr = choiceArr.concat(numberArr);
+  }
+  return true;
+}}
